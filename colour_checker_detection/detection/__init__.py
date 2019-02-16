611498f6aaf8ab6cb5a139971a8c6b6989a6c9c1
Manuel Leonhardt <skycaptain@users.noreply.github.com>
2019-02-16 19:52:55 +0100

    Fix SyntaxError
    Fixes missing `'` causing SyntaxError: EOL while scanning string literal

--- colour_checker_detection/detection/__init__.py
+++ colour_checker_detection/detection/__init__.py
@@ -9,5 +9,5 @@ from .segmentation import (colour_checkers_coordinates_segmentation,
 __all__ = [
     'colour_checkers_coordinates_segmentation',
     'extract_colour_checkers_segmentation',
-    'detect_colour_checkers_segmentation
+    'detect_colour_checkers_segmentation'
 ]
