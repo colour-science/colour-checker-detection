9157ea8dcb607d8e7ca099f3caf57ec5251eee49
Thomas Mansencal <thomas.mansencal@gmail.com>
2021-01-01 14:27:48 +1300

    Update copyright statements.

--- colour_checker_detection/__init__.py
+++ colour_checker_detection/__init__.py
@@ -22,7 +22,7 @@ from .detection import (colour_checkers_coordinates_segmentation,
                         detect_colour_checkers_segmentation)
 
 __author__ = 'Colour Developers'
-__copyright__ = 'Copyright (C) 2018-2020 - Colour Developers'
+__copyright__ = 'Copyright (C) 2018-2021 - Colour Developers'
 __license__ = 'New BSD License - https://opensource.org/licenses/BSD-3-Clause'
 __maintainer__ = 'Colour Developers'
 __email__ = 'colour-developers@colour-science.org'
--- tasks.py
+++ tasks.py
@@ -15,7 +15,7 @@ import colour_checker_detection
 from colour.utilities import message_box
 
 __author__ = 'Colour Developers'
-__copyright__ = 'Copyright (C) 2018-2020 - Colour Developers'
+__copyright__ = 'Copyright (C) 2018-2021 - Colour Developers'
 __license__ = 'New BSD License - https://opensource.org/licenses/BSD-3-Clause'
 __maintainer__ = 'Colour Developers'
 __email__ = 'colour-developers@colour-science.org'
